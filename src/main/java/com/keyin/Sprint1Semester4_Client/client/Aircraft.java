c07ed0ddff29f2ff84229542d0cc01e88c8ebaf6
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-12 20:44:27 -0230

    I've built 5 files for client folder
    I've built the main, aircraft, airport, city, and Passenger files.

---
+++ src/main/java/com/keyin/Sprint1Semester4_Client/client/Aircraft.java
@@ -0,0 +1,10 @@
+package com.keyin.Sprint1Semester4_Client.client;
+
+public class Aircraft {
+    private Long id;
+    private String type;
+    private String airlineName;
+    private int numberOfPassengers;
+
+    // Constructors, getters, and setters
+}
